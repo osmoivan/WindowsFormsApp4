87a5ab7d695902dd3d9a86473b9019f411134c06
osmoivan <34392866+osmoivan@users.noreply.github.com>
2017-12-15 22:58:39 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.создатьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьКакToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.проектToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.добавитьДокументToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.удалитьДокументToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.удалитьВсеДокументыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(146, 28);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(120, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(13, 70);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(122, 394);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(305, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(74, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(404, 28);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Добавить СФ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(516, 28);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(87, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Добавить СФ";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(148, 72);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.textBox1.Size = new System.Drawing.Size(454, 391);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(13, 28);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(122, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Удалить СФ";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.проектToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(613, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.создатьToolStripMenuItem,
+            this.открытьToolStripMenuItem,
+            this.сохранитьКакToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // создатьToolStripMenuItem
+            // 
+            this.создатьToolStripMenuItem.Name = "создатьToolStripMenuItem";
+            this.создатьToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.создатьToolStripMenuItem.Text = "Создать";
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.открытьToolStripMenuItem.Text = "Открыть";
+            // 
+            // сохранитьКакToolStripMenuItem
+            // 
+            this.сохранитьКакToolStripMenuItem.Name = "сохранитьКакToolStripMenuItem";
+            this.сохранитьКакToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.сохранитьКакToolStripMenuItem.Text = "Сохранить как";
+            // 
+            // проектToolStripMenuItem
+            // 
+            this.проектToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.добавитьДокументToolStripMenuItem,
+            this.удалитьДокументToolStripMenuItem,
+            this.удалитьВсеДокументыToolStripMenuItem});
+            this.проектToolStripMenuItem.Name = "проектToolStripMenuItem";
+            this.проектToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.проектToolStripMenuItem.Text = "Проект";
+            // 
+            // добавитьДокументToolStripMenuItem
+            // 
+            this.добавитьДокументToolStripMenuItem.Name = "добавитьДокументToolStripMenuItem";
+            this.добавитьДокументToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.добавитьДокументToolStripMenuItem.Text = "Добавить документ";
+            this.добавитьДокументToolStripMenuItem.Click += new System.EventHandler(this.добавитьДокументToolStripMenuItem_Click);
+            // 
+            // удалитьДокументToolStripMenuItem
+            // 
+            this.удалитьДокументToolStripMenuItem.Name = "удалитьДокументToolStripMenuItem";
+            this.удалитьДокументToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.удалитьДокументToolStripMenuItem.Text = "Удалить документ";
+            this.удалитьДокументToolStripMenuItem.Click += new System.EventHandler(this.удалитьДокументToolStripMenuItem_Click);
+            // 
+            // удалитьВсеДокументыToolStripMenuItem
+            // 
+            this.удалитьВсеДокументыToolStripMenuItem.Name = "удалитьВсеДокументыToolStripMenuItem";
+            this.удалитьВсеДокументыToolStripMenuItem.Size = new System.Drawing.Size(203, 22);
+            this.удалитьВсеДокументыToolStripMenuItem.Text = "Удалить все документы";
+            this.удалитьВсеДокументыToolStripMenuItem.Click += new System.EventHandler(this.удалитьВсеДокументыToolStripMenuItem_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(613, 481);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+		public  System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem создатьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьКакToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem проектToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem добавитьДокументToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem удалитьДокументToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem удалитьВсеДокументыToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Word = Microsoft.Office.Interop.Word; //Введение алиаса пространства имен Word
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = "1111qwertyuiop[]asdfghjklzxcvbnm";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] x = new int[2];
+            x = Ivan.SelectedTextIntoIndexForSemanticFragmentTable(textBox1);
+            Ivan.AddIndexIntoSemanticFragmentTable("C://Users/ivan_/Desktop/time.txt");
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Sergey.ComboBox1_SelectedIndexChanged(comboBox1,textBox1);          //Переход между документами в комбобоксе
+        }
+
+        private void удалитьДокументToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Sergey.удалитьФайлToolStripMenuItem_Click(comboBox1, textBox1);    //Удаление текущего документа (по значению в комбобоксе)
+        }
+
+        private void добавитьДокументToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();             //Открытие диалогового окна для подгрузки текста из файла
+            Sergey.добавитьФайлToolStripMenuItem_Click(textBox1, comboBox1, openFileDialog);
+        }
+
+        private void удалитьВсеДокументыToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            Sergey.удалитьВсеДокументыToolStripMenuItem_Click(comboBox1, textBox1);  //Удаление всех существующих документов
+        }
+    }
+}
+public class Ivan
+{
+    private static int[] outdata = new int[2];
+    public static int[] SelectedTextIntoIndexForSemanticFragmentTable(System.Windows.Forms.TextBox textBox)
+    {
+        if (textBox.SelectionLength > 0)
+        {
+            outdata[0] = textBox.SelectionStart;
+            outdata[1] = textBox.SelectionStart + textBox.SelectionLength - 1;
+            return outdata;
+        }
+        else
+        {
+            MessageBox.Show("Вы не выделили смысловой фрагмент");
+            return outdata;
+        }
+    }
+    private static int[,] ReadInformFromSemanticFragmentTable(string File)
+    {
+        int lengthTable = System.IO.File.ReadAllLines(File).Length + 1;
+        int[,] index = new int[lengthTable, 2];
+        int ind = 0;
+        using (StreamReader sr = new StreamReader(File))   //System.IO.File.Create(File))
+        {
+            while (sr.Peek() >= 0)
+            {
+                string s = sr.ReadLine();
+                string[] s1 = s.Split('\t');
+                index[ind, 0] = Convert.ToInt32(s1[1]);
+                index[ind, 1] = Convert.ToInt32(s1[2]);
+                ind = ind + 1;
+            }
+        }
+        return index;
+    }
+    private static void WriteInformIntoSemanticFragmentTable(string File, int[,] index)
+    {
+        using (StreamWriter sw = new StreamWriter(File))
+        {
+            for (int i = 0; i < index.Length / 2; i++)
+            {
+                sw.WriteLine("СФ" + i.ToString() + "\t" + index[i, 0].ToString() + "\t" + index[i, 1].ToString());
+            }
+        }
+    }
+    public static void AddIndexIntoSemanticFragmentTable(string File)
+    {
+        int[,] index = ReadInformFromSemanticFragmentTable(File);
+        index = Ivan.SortMatrix(index);
+        index[0, 0] = outdata[0];
+        index[0, 1] = outdata[1];
+        index=Ivan.CheckCrossingElements(index);
+        WriteInformIntoSemanticFragmentTable(File, index);
+    }
+    private static int[,] SortMatrix(int[,] index)
+    {
+        List<int> FirstElement = new List<int>();
+        List<int> SecondElement = new List<int>();
+        int[,] timeindex = new int[index.GetLength(0), index.GetLength(1)];
+        for (int i = 0; i < index.GetLength(0); i++)
+        {
+            FirstElement.Add(index[i, 0]);
+            SecondElement.Add(index[i, 1]);
+        }
+        for (int i = 0; i < index.GetLength(0); i++)
+        {
+            int c = FirstElement.IndexOf(FirstElement.Min());
+            timeindex[i, 0] = FirstElement[c];
+            timeindex[i, 1] = SecondElement[c];
+            FirstElement.RemoveAt(c);
+            SecondElement.RemoveAt(c);
+        }
+        return timeindex;
+    }
+    //private static int[,] RemoveOneElement(int [,]index)
+    //{
+
+    //}
+    private static int[,] CheckCrossingElements(int[,] index)
+    {
+        if (index.Length > 2)
+        {
+            //Если новый интервал полоностью покрывает старый
+            if (index[0, 0]<=index[1,0] && index[1,0]>=index[0,0])
+            {
+
+            }
+        }
+        return index;
+    }
+}
+public class Sergey
+{
+    static private Word.Application S_wordapp;
+    static private Word.Document S_worddocument;
+    static string S_namefiledirect;
+    static string S_namefile;
+    static string S_text;
+    static List<string> S_nameFile = new List<string>();
+    static List<string> S_textFile = new List<string>();
+
+    public static void добавитьФайлToolStripMenuItem_Click(System.Windows.Forms.TextBox textBox, System.Windows.Forms.ComboBox comboBox, System.Windows.Forms.OpenFileDialog openFileDialog1)
+    {
+        
+        if (openFileDialog1.ShowDialog() == DialogResult.OK)                           //Открытие диалогового окна для открытия файла
+        {
+            S_namefiledirect = openFileDialog1.FileName;                               //Сохранение в переменную пути к выбранному фалу
+            S_namefile = openFileDialog1.SafeFileName;                                 //Сохранение в переменную имени выбранного файла
+            S_wordapp = new Word.Application();                                        //Создаем объект Word - равносильно запуску Word.
+            S_wordapp.Visible = false;
+            Object filename = S_namefiledirect;
+            S_worddocument = S_wordapp.Documents.Open(ref filename);                   //Открываем конкретный существующий word документ из нужной директории.
+            Object begin = Type.Missing;                                               //В документе определяем диапазон,вызовом метода Range  
+            Object end = Type.Missing;                                                 //с передачей ему начального 
+            Word.Range wordrange = S_worddocument.Range(ref begin, ref end);           //и конечного значений позиций символов.
+            wordrange.Copy();                                                          //Копирование в буфер обмена.
+            S_text = Clipboard.GetText();                                              //Сохранение в переменную скопированного текста
+            textBox.Text = S_text;                                                    //Извлекаем из буфера обмена копированный текст.
+            comboBox.Items.Add(S_namefile);                                           //Добавление в combobox1 имени файла, с которого мы скопировали текст 
+            comboBox.Text = S_namefile;                                               //Надпись комбобокса меняется на имя файла
+            S_nameFile.Add(S_namefile);                                                //Добавление в список имени файла
+            S_textFile.Add(S_text);                                                    //Добавление в список текста файла
+            Object saveChanges = Word.WdSaveOptions.wdPromptToSaveChanges;
+            Object originalFormat = Word.WdOriginalFormat.wdWordDocument;
+            Object routeDocument = Type.Missing;
+            S_wordapp.Quit(ref saveChanges, ref originalFormat, ref routeDocument);    // Закрытие файла
+            S_wordapp = null;
+        }
+    }
+
+
+    public static void ComboBox1_SelectedIndexChanged(System.Windows.Forms.ComboBox comboBox,System.Windows.Forms.TextBox textBox)
+    {
+        for (int i = 0; i < S_nameFile.Count; i++)                                 
+        {
+            if (comboBox.Text == S_nameFile[i])
+            {
+                textBox.Text = S_textFile[i];
+            }
+        }
+    }
+
+    public static void удалитьФайлToolStripMenuItem_Click(System.Windows.Forms.ComboBox comboBox,System.Windows.Forms.TextBox textBox)
+    {
+        for (int i = 0; i < S_nameFile.Count; i++)
+        {
+            if (MessageBox.Show("Удалить текущий документы?", "Удаление", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2) == DialogResult.Yes)
+                if (comboBox.Text == S_nameFile[i])
+                {
+                    comboBox.Items.Remove(S_nameFile[i]);
+                    S_nameFile.RemoveAt(i);
+                    S_textFile.RemoveAt(i);
+                    comboBox.Text = "Файл";
+                    textBox.Text = "";
+            }
+        }
+    }
+    public static void удалитьВсеДокументыToolStripMenuItem_Click(System.Windows.Forms.ComboBox comboBox,System.Windows.Forms.TextBox textBox)  
+    {
+        if (MessageBox.Show("Удалить все существующие документы?", "Удаление", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2) == DialogResult.Yes)
+        {
+            comboBox.Items.Clear();
+            comboBox.Text = "Файл";
+            S_nameFile.Clear();
+            S_textFile.Clear();
+            textBox.Text = "";
+        }
+    }
+}
